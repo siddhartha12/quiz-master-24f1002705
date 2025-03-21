e35c17b962e876a5d79a7edf19c311346ef2536f
Siddhartha Varma Chittari <siddhartha12122002@gmail.com>
2025-03-21 12:25:26 +0530

    posted for register, need to initialize sqlite

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['GET'])
 def register():
     return render_template('registration.html')
+
+@app.route('/register', methods=['POST'])
+def register_details():
+    return render_template('registered.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
